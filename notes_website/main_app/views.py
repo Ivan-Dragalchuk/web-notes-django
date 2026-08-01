a4d16f982cb848fc15159c51393009a9af04df11
Ivan-Dragalchuck <dragalchukivan4@gmail.com>
2026-08-01 15:26:59 +0300

    make BD

--- notes_website/main_app/views.py
+++ notes_website/main_app/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from .forms import AddNewNote
+from .models import AddNewNoteModel
 from django.http import HttpResponseRedirect
 
 
 def menu(request):
-    return render(request, "main_app/index.html")
+    notes = AddNewNoteModel.objects.all()
+    return render(request, "main_app/index.html", context={
+        "notes":notes,
+    })
 
 def add_(request):
     if request.method == "POST":
         form = AddNewNote(request.POST)
         if form.is_valid():
+            addnote = AddNewNoteModel(
+            title = form.cleaned_data["title"],
+            describe = form.cleaned_data["describe"],
+            )
+            addnote.save()
             return HttpResponseRedirect("/menu")
     else:
         form = AddNewNote()
